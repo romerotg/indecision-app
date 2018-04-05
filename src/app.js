2e6a621cb647b15ca33cb93ac6bc2978b26caab1
Romero Gonçalves <romerotg@gmail.com>
2018-04-05 18:13:51 -0300

    Commit inicial

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = <p>Indecision App</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);

2f10affc4fc09d41154c22a0ed5da6d56e5dce81
Romero Gonçalves <romerotg@gmail.com>
2018-04-06 11:51:04 -0300

    classes e playground

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,76 @@
 
 console.log('App.js is running!');
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
 // JSX - JavaScript XML
 var template = React.createElement(
-  'p',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ul',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
+    )
 );
+
+var user = {
+    name: 'Romero Gonçalves',
+    age: 28,
+    location: 'Recife'
+};
+function getLocation(location) {
+    if (location) return React.createElement(
+        'p',
+        null,
+        'Location: ',
+        location
+    );
+}
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
 console.log('App.js is running!');
 
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
 // JSX - JavaScript XML
-var template = <p>Indecision App</p>;
-var appRoot = document.getElementById('app');
+const template =
+(
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ul>
+    </div>
+);
+
+const user =
+{
+    name: 'Romero Gonçalves',
+    age: 28,
+    location: 'Recife'
+};
+function getLocation(location)
+{
+    if (location)
+        return <p>Location: {location}</p>;
+}
+const template2 = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
